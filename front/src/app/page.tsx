2c3597ce1200e75a2829d057ad1457d66a9bb90c
SHO800 <fg582323@docomo.ne.jp>
2025-10-06 00:10:49 +0900

    [update] /にエフェクトをいくつか追加

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,16 +1,53 @@
-"use client"
+import Image from "next/image";
 import { Modal } from "@/components/Modal";
 import { MapPinHouseIcon } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="p-10">
-      {/* ボタンはコンポーネントに埋めてあるので、こちらからは中身のみ渡す */}
-      <Modal button={<MapPinHouseIcon/>}>
-        <li>hoge</li>
-        <li>huga</li>
-        <li>poo</li>
-      </Modal>
-    </div>
-  );
+
+    return (
+        <div className={"relative "}>
+            <main
+                className={"flex min-h-screen flex-col items-center justify-between p-24 bg-[hsl(202,100%,94%)] bg-no-repeat bg-(image:--mesh-gradient) "}>
+                {/*className={"flex min-h-screen flex-col items-center justify-between p-24 bg-linear-to-br/hsl from-[hsl(195,90%,91%)] to-[hsl(243,81%,78%)] "}>*/}
+                
+                <div
+                    className="h-screen w-screen ">
+                    <div className="flex h-screen flex-col items-center justify-center bg-gray-800 sun">
+                        <h1 className="text-6xl font-bold bg-gray-800 load-title">Kosen-sai</h1>
+                    </div>
+                </div>
+            </main>
+
+                
+            <div className={"relative w-screen h-screen"}>
+                <Image
+                    src={"/top/poster/poster-full-textless.webp"}
+                    alt={"ポスター"}
+                    fill={true}
+                    
+                    className={"absolute w-full! h-full! object-cover "}
+            
+            
+                />
+                
+                {/*この要素で制御*/}
+                {/*<div className={"fixed top-0 left-0 w-screen h-screen bg-black  mask-radial-at-top-left mask-radial-from-0 mask-radial-to-100%"}>*/}
+                {/*</div>*/}
+            </div>
+            
+            
+        </div>
+    );
+    
+  // モーダル使用例
+  // return (
+  //   <div className="p-10">
+  //     {/* ボタンはコンポーネントに埋めてあるので、こちらからは中身のみ渡す */}
+  //     <Modal button={<MapPinHouseIcon/>}>
+  //       <li>hoge</li>
+  //       <li>huga</li>
+  //       <li>poo</li>
+  //     </Modal>
+  //   </div>
+  // );
 }
