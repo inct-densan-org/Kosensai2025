e1bddfcef23597bed9a6d479bcc7862342e9c6ef
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-10-10 14:30:24 +0900

    [update] Navigationの左上をHomeに変更し, 押すとヒーローセクションにスクロールするよう改修

--- front/src/components/top/Navigation.tsx
+++ front/src/components/top/Navigation.tsx
@@ -5,9 +5,9 @@ import {dotGothic16Font} from "@/app/layout";
 export default function Navigation() {
     return (
         <nav
-            className={"sticky bg-blur-sm h-12 w-auto mx-4 mt-4  top-4 backdrop-blur-sm border-gray-800 border-[1px] rounded-xl flex items-center px-4 gap-2"}>
-            <a className={dotGothic16Font.className + " text-md tracking-wide text-foreground mr-auto"}
-               ><ScrollToTop>高専祭2025</ScrollToTop></a>
+            className={"sticky bg-blur-sm h-12 w-auto mx-4 mt-4  top-4 backdrop-blur-sm border-gray-800 border-[1px] rounded-xl flex items-center px-4 gap-2 justify-around"}>
+            <a className={dotGothic16Font.className + " text-md tracking-wide text-foreground"}
+               ><ScrollToTop>HOME</ScrollToTop></a>
             <a className={dotGothic16Font.className + " text-sm tracking-wide text-foreground "}>NEWS</a>
             <a className={dotGothic16Font.className + " text-sm tracking-wide text-foreground "}>SHOPS</a>
             <a href={"/map"} className={dotGothic16Font.className + " text-sm tracking-wide text-foreground "}>MAP</a>
