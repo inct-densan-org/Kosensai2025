06f2339dba79b5b1b97b9e6e8f45d1f26c936147
Nogu-y <yoichinogu5@gmail.com>
2025-10-14 20:14:12 +0900

    [add] microCMSとフロントを中継するAPIを作成

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,11 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { zValidator } from '@hono/zod-validator'
+import { CMS_client as client } from './utils.js'
 //↑モジュールたち
 //↓型定義ファイル
-import { request } from './schema.js'
+import { request, type NewsData, type NewsList } from './schema.js'
+import z from 'zod'
 
 const app = new Hono()
   .use(cors({
@@ -16,6 +18,29 @@ const app = new Hono()
   .post("/post",zValidator("json",request), (c)=>{
     const data = c.req.valid("json")
     return c.json({ message: `${data.name} is ${data.age} years old`})
+  })
+
+  .get('/news',async (c)=>{
+      const data = (await client.get({
+        endpoint: "list"
+      }))["contents"] as NewsData[]
+      data.map(i=>({
+        id:i.id,
+        title:i.title,
+        description:i.description,
+        createdAt:i.createdAt
+      }))
+      data as NewsList[]
+      return c.json( {data}, 200 )
+  })
+
+  .get("news/:id", zValidator( "param", z.object({id:z.string()}) ), async (c)=>{
+    const id = c.req.valid("param").id
+    const data = await client.get({
+      endpoint: 'list',
+      contentId: id,
+    }) as NewsData
+    return c.json( {data}, 200)
   })
 
 export type AppType = typeof app  
--- api/src/schema.ts
+++ api/src/schema.ts
@@ -4,3 +4,19 @@ export const request = z.object({
   name: z.string(),
   age: z.number()
 })
+
+export type NewsData = {
+  id:string
+  title:string
+  description:string|null
+  body:string
+  createdAt:string
+  updatedAt:string
+}
+
+export type NewsList = {
+  id:string
+  title:string
+  description:string|null
+  createdAt:string
+}
