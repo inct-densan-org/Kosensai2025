c4c21e9f6e95a7933592e663e11c00c2e88b66e4
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-10-17 17:30:35 +0900

    [update] Imageのコンテナか否かに応じてフィットさせられるよう改修

--- front/src/components/MotionDivInClient.tsx
+++ front/src/components/MotionDivInClient.tsx
@@ -4,12 +4,14 @@ import {motion, MotionProps} from "motion/react"
 import {ReactNode} from "react";
 
 type Props = {
+    isImg: boolean
     children: ReactNode
 } & MotionProps
 
-export const MotionDivInClient = ({children, ...props}: Props) => {
+export const MotionDivInClient = ({isImg = false, children, ...props}: Props) => {
     return (
         <motion.div
+            className={isImg? "absolute w-screen! h-full! object-cover after:pointer-events-none  ": ""}
             {...props}
         >
             {children}
