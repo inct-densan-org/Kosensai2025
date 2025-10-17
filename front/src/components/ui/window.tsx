61e57133d26b58f11090c554aae8eeb59b1bc820
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-10-17 17:31:15 +0900

    [update] windowのデザイン, 及び文字サイズを変更

--- front/src/components/ui/window.tsx
+++ front/src/components/ui/window.tsx
@@ -1,18 +1,20 @@
-import {ReactElement, ReactNode} from "react";
+import {CSSProperties, ReactNode} from "react";
 
-export function Window({title, children, subtitle}: {title: string, children: ReactNode,  subtitle?: string}) {
+
+export function Window({title, children, color, subtitle, className}: { title: string, children: ReactNode, color?:CSSProperties["color"] , subtitle?: string, className?: string }) {
     return (
-        <div className={"border-black border-[1px] rounded-sm m-8 w-auto h-fit bg-[rgba(255,255,255,0.5)]  text-foreground   font-['Mplus 1p'] "}>
+        <div className={"relative  border-white border-[1px] rounded-sm m-8 w-auto font-['Mplus 1p'] md:text-[1.2rem] overflow-y-hidden bg-[rgba(102,102,102,0.15)] backdrop-blur-2xl z-[200] flex flex-col " + className}
+        style={{color: color}}>
             {/*上部のフチ*/}
-            <div className={"px-3 w-full py-1/2 h-7 bg-  rounded-t-sm border-b-black border-[1px] font-bold flex flex-row items-end gap-2"}>
-                <h3>{title}</h3>
-                {subtitle && <p className={"pb-[.2em] pl-2%] text-[.6em] font-normal"}>{subtitle}</p>}
+            <div
+                className={"px-3 w-full py-1/2 h-7  rounded-t-sm border-b-white border-[1px] font-bold flex flex-row items-end gap-2 text-white"}>
+                <h3 className={"md:text-[1.2rem]"}>{title}</h3>
+                {subtitle && <p className={"pb-[.2em] pl-[2%] text-[.6em] md:text-[.9rem] md:pb-0 font-normal"}>{subtitle}</p>}
             </div>
-            
+
             {/*コンテンツ*/}
-            <div className={"px-4 pt-2 pb-4 text-[0.875rem] leading-[1.5rem]"}>
-            {children}
-                
+            <div className={"px-4 pt-2 pb-4 text-[0.875em] leading-[1.5rem] h-full overflow-y-scroll hidden-scrollbar"}>
+                {children}
             </div>
         </div>
     )
