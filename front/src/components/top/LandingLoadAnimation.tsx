6a01301e40074a184d48a9ac144e5b3c5106e4fb
SHO800 <fg582323@docomo.ne.jp>
2025-10-19 15:59:32 +0900

    [add] セクションをいい感じに現れるようにするコンポーネントを追加

--- front/src/components/top/LandingLoadAnimation.tsx
+++ front/src/components/top/LandingLoadAnimation.tsx
@@ -1,6 +1,6 @@
 export function LandingLoadAnimation() {
     return (
-        <div className="fixed inset-0 h-screen w-screen z-50 overflow-hidden hidden-scrollbar ">
+        <div className="fixed inset-0 h-screen w-screen overflow-hidden hidden-scrollbar z-[999] ">
             <div className="flex h-screen flex-col items-center justify-center bg-dark-background sun after:animate-caret-blink">
                 <h1 className="text-6xl font-bold bg-dark-background load-title">Kosen-sai</h1>
             </div>
