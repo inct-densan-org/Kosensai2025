23a89fe3d405d50181bcee621742e46977ee216c
SHO800 <fg582323@docomo.ne.jp>
2025-10-20 01:52:07 +0900

    [update] favicon, titleを設定

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,12 +2,19 @@ import "./globals.css";
 import {DotGothic16} from "next/font/google";
 import {Suspense} from "react";
 import Loading from "@/components/Loading";
+import {Metadata} from "next";
 
 
 const dotGothic16Font = DotGothic16({
     weight: "400",
     subsets: ["latin"],
 })
+
+export const metadata: Metadata = {
+    title: "一関工業高等専門学校 高専祭2025",
+    description: "一関工業高等専門学校にて開催される第61回高専祭の特設サイトです。10月25日(土)、26日(日)の午前9時から午後5時まで開場しております。ぜひご参加ください。",
+};
+
 
 export default function RootLayout({
                                        children,
