082b08c1f0633e3d95852e569347882fdb6f200a
SHO800 <fg582323@docomo.ne.jp>
2026-06-26 21:53:12 +0900

    [update] OG画像を追加

--- front/src/app/ccd-map/layout.tsx
+++ front/src/app/ccd-map/layout.tsx
@@ -9,7 +9,11 @@ export const metadata: Metadata = {
     openGraph: {
         type: "website",
         locale: "ja_JP",
-
+        images: [{
+            url: "https://www.ichinoseki.ac.jp/files/63bdc0c003c6ce281314e90d4a55c9f30.jpg",
+            width: 512,
+            height: 512
+        }],
         title: "キャンパスカミングデー2026 - 一関工業高等専門学校 ",
         siteName: "一関工業高等専門学校 R8キャンパスカミングデー",
         description: "2026年6月27日に行われるキャンパスカミングデー用の特設ページです。"
@@ -18,8 +22,6 @@ export const metadata: Metadata = {
     twitter: {
         card: "summary_large_image",
     },
-
-
 };
 
 export default function CCDMapLayout({children}: { children: React.ReactNode }) {
